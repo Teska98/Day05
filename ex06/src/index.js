b088ca3422d65ec51bc18e405fc6018c2aad33b9
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-13 00:15:05 +0100

    Update index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,7 +1,7 @@
 function main(){
     // Only change code below this line
-       var myName = "Teskeredzic"
-       var myString = "My name is " + myName + ", and I am well!"
+       var myName = "Teskeredzic";
+       var myString = "My name is " + myName + ", and I am well!";
     // Only chane code above this line
  return myString;
 }
