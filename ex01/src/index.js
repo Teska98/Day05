86e0f3f4e1559fbe044686864160bf33b4c1da21
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-12 23:08:36 +0100

    Create index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+     var myFirstString = "i think therefore I \"am \" -- sentient.";
+     var mySecondString = "No, you only \"think\", so you aren˙t."
+    // Only chane code above this line
+ return {
+   myFirstString,
+   mySecondString
+ };
+}
+console.log(main());
+module.exports = main;
