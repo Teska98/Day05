744fd07be73e3b61b0cfdfc7f2dda8a824c1f2b5
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-13 00:24:16 +0100

    Create index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // Only change code below this line
+     var lastName = "Doe";
+     var firstLetterLastName = " ";
+     firstLetterLastName = lastName[0];
+    // Only chane code above this line
+ return firstLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
